093d9c1a2dd6d54fcad77aef2225990fca8da499
Nicholas Rahbany <nrahbany22@yahoo.com>
2018-04-25 15:15:44 -0400

    Added code that Tourini was displaying

--- Assignments/Assignment 1/code/TennisPlayer.java
+++ Assignments/Assignment 1/code/TennisPlayer.java
@@ -5,9 +5,18 @@ Nicholas Rahbany
 CS 102
 */
 
-public class TennisPlayer {
+public class TennisPlayer implements TennisPlayerInterface {
 
-    public TennisPlayer() {
+    private String id; // Unique Key, Read-only
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+    private String country;
+    private boolean dummy;
+    private TennisPlayerWinLossRecord record; // Read-only
+
+    // constructors
+    public TennisPlayer(String id, String firstName, String lastName, int birthYear, String country, boolean dummy, TennisPlayerWinLossRecord record) {
         throw NotImplementedException;
     }
 
