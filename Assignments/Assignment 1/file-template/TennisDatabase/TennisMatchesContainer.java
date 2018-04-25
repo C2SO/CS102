6638c4e46745ebc8ba0e8da2760389424ca92590
Nicholas Rahbany <nrahbany22@yahoo.com>
2018-04-25 18:27:38 -0400

    Added specific code from Tourini

--- Assignments/Assignment 1/file-template/TennisDatabase/TennisMatchesContainer.java
+++ Assignments/Assignment 1/file-template/TennisDatabase/TennisMatchesContainer.java
@@ -1,7 +1,29 @@
 package TennisDatabase;
 
-public class TennisMatchesContainer implements TennisMatchesContainerInterface {
-    public void insertMatch( TennisMatch m ) {}
+import TennisDatabase.TennisDatabaseRuntimeException;
 
-    public void printAllMatches() {}
+public class TennisMatchesContainer implements TennisMatchesContainerInterface {
+
+    private TennisMatch[] tma;
+    private int sizeLogical;
+    private int sizePhysical;
+
+    public TennisMatchesContainer() {
+        sizePhysical = 2;
+        sizeLogical = 0;
+        tma = new TennisMatch[sizePhysical];
+    }
+
+    public void insertMatch(TennisMatch m) {
+    }
+
+    public void printAllMatches() throws TennisDatabaseRuntimeException {
+        if (tma.length == 0) {
+            throw new TennisDatabaseRuntimeException("No Tennis Matches Available");
+        } else {
+            for (int i = 0; i < sizeLogical; i++) {
+                tma[i].print();
+            }
+        }
+    }
 }
